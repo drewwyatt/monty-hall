177d9240aa1bf7bfc2ca8e73d7dc00652a873eef
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-02-08 23:39:31 -0500

    list stuff

--- src/simulate.ts
+++ src/simulate.ts
@@ -1,15 +1,20 @@
 import Listr from 'listr'
 import { range } from 'ramda'
 
-const simulate = (numberOfTrials: number, shouldSwitch: boolean) => {
+const simulate = async (numberOfTrials: number, shouldSwitch: boolean) => {
   const tasks = new Listr(
     range(0, numberOfTrials).map(idx => ({
       title: `Trial #${idx + 1}`,
-      task: () => new Promise(r => setTimeout(() => r(), 1000 + idx)),
+      task: () =>
+        new Promise((r, e) =>
+          setTimeout(() => (idx % 2 === 0 ? r() : e(new Error())), 200 + idx),
+        ),
     })),
+    { concurrent: 5, exitOnError: false },
   )
 
-  tasks.run()
+  const ctx = tasks.run().catch(() => null)
+  // console.log('context:', ctx)
 }
 
 export default simulate
